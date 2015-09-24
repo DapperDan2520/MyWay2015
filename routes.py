1760ed2bc7f934ed97c550ddff6c31563c078f20
George <geo.offley@gmail.com>
2015-09-23 22:07:14 -0400

    started with sign up

--- routes.py
+++ routes.py
@@ -8,9 +8,21 @@ import db
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+'''class UserNotFoundError(Exception):
+	pass
+
+class User(UserMixin):
+	
+	def __init__(self, id):
+		if not id in self.
+'''
+
 @app.route('/')
 def main():
 	return render_template('index.html')
+
+@app.rout('/signup_check', methods=['POST']):
+
 
 if __name__ == "__main__":
     app.run(debug=True)
